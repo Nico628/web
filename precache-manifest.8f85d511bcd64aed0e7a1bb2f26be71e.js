5ba9d24f4f12300dabc2611e281ebe85e1438743
Nico Chan <parcseechan@yahoo.com>
2019-08-19 18:30:49 -0700

    Updates

---
+++ precache-manifest.8f85d511bcd64aed0e7a1bb2f26be71e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcf3c2817e6f548adf61e349243482c0",
+    "url": "/web/index.html"
+  },
+  {
+    "revision": "ba2170878eee45c649dd",
+    "url": "/web/static/css/main.3e08507d.chunk.css"
+  },
+  {
+    "revision": "eabd0a316f41ab75b61e",
+    "url": "/web/static/js/2.df8dace8.chunk.js"
+  },
+  {
+    "revision": "ba2170878eee45c649dd",
+    "url": "/web/static/js/main.7889a7f7.chunk.js"
+  },
+  {
+    "revision": "33ed4ec846e9d84e8327",
+    "url": "/web/static/js/runtime~main.c75ffd02.js"
+  }
+]);
